0464560f784f65ae6e14b1d1afe4c009fa5e357f
Webby Ayush <ayush2210019-d@akgec.ac.in>
2025-01-12 22:03:32 +0530

    bug fixes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Header from "./components/header";
 import SubHeader from "./components/SubHeader";
-import Table from "./components/DynamicTable";
 
 export default function Home() {
   return (
